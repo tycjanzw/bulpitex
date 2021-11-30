7536891c95dc6016c9e66ae9f66c105c90469f8d
piotrstukus@outlook.com <piotrstukus@outlook.com>
2021-11-30 10:54:07 +0100

    Room - poczatek

--- Bulpitex/static/room.js
+++ Bulpitex/static/room.js
@@ -1,11 +1,33 @@
 var peerCon = null;
-var currentPeer = null
+var currentPeerCon = null
 var myStream;
+var roomID;
+
 
 var createRoomBtn = document.getElementById('createRoomBtn');
 
 createRoomBtn.addEventListener('click', createRoom);
 
 function createRoom(){
-    alert('pokój');
+    roomID = generateCode();
+    peerCon = new Peer(roomID);
+    
+    peerCon.on('open', id=>{
+        
+    });
+    
+    peerCon.on('call', call=>{
+        
+    });
+    
+}
+
+
+function joinRoom(){
+    roomID = document.getElementById('peerID');
+    peerCon= new Peer();
+
+    peerCon.peerCon('open', id=>{
+
+    });
 }
