575e57f8f9c17a5500fe83de05e2bf83824f0d40
piotrstukus@outlook.com <piotrstukus@outlook.com>
2021-12-03 10:31:49 +0100

    Room - v2

--- Bulpitex/static/room.js
+++ Bulpitex/static/room.js
@@ -3,7 +3,6 @@ var currentPeerCon = null
 var myStream;
 var roomID;
 
-
 var createRoomBtn = document.getElementById('createRoomBtn');
 
 createRoomBtn.addEventListener('click', createRoom);
@@ -12,12 +11,24 @@ function createRoom(){
     roomID = generateCode();
     peerCon = new Peer(roomID);
     
+    $('#sendPeerCode').click(()=>{
+        var kod = document.getElementById('showPeer').innerHTML;
+        socket.emit('sendCode',{p:kod});
+    });
+
+    socket.on('setPeerCode', function(c){
+        document.getElementById('r').innerHTML = c.pCode;
+    });
+
     peerCon.on('open', id=>{
-        //
+        $('#showPeer').html(id);
     });
     
     peerCon.on('call', call=>{
-        //
+        call.answer(myStream);
+            call.on('stream', function(remoteStream){
+            addRemoteVideo(remoteStream);
+        });
     });
     
 }
@@ -29,5 +40,29 @@ function joinRoom(){
 
     peerCon.peerCon('open', id=>{
 
+    });
+}
+
+
+function callPeer(id){
+    navigator.mediaDevices.getDisplayMedia({
+        video:{
+            cursor: "always"
+        }
+    }).then((stream)=>{
+        myStream = stream;
+        let call = peer.call(id, stream);
+        call.on('stream', function(remoteStream){
+            if(!peerList.includes(call.peer)){
+                //addRemoteVideo(remoteStream);
+                currentPeer = call.peerConnection;
+                peerList.push(call.peer);
+            }
+        })
+        stream.getVideoTracks()[0].addEventListener('ended', () => {
+            reconnectStream();
+        });
+    }).catch((err)=>{
+        console.log(err+" nie ma streamu");
     });
 }
