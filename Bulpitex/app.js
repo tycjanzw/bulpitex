459ac7e52f3ceeb60785e2e007bbba07297ebea6
Sopata_Rafal <sopaciak@gmail.com>
2021-12-06 17:58:48 +0100

    refaktoryzacja_klawiszy

--- Bulpitex/app.js
+++ Bulpitex/app.js
@@ -54,13 +54,17 @@ io.on('connection', (socket) => {
         robot.mouseClick('left',true);
     });
 
+    // KLIKNIĘCIE KLAWISZEM
     socket.on('sendPressKey', keyData=>{
         //console.log(keyData.k);
-        pressKey(keyData.k);
+        //pressKey(keyData.k);
+        setKeyToogle(keyData.k, "down");
     });
 
+    // ZWOLNIENIE KLAWISZEM
     socket.on('sendUpKey', keyData=>{
-        upKey(keyData.k);
+        //upKey(keyData.k);
+        setKeyToogle(keyData.k, "up");
     });
 
     socket.on('getCode', () => {
@@ -101,208 +105,101 @@ io.on('connection', (socket) => {
 
 httpServer.listen(PORT);
 
-function pressKey(key){
+function setKeyToogle(key, val){
     if(key === 'Enter'){ 
         try {
-            robot.keyToggle("enter", "down");
+            robot.keyToggle("enter", val);
         } catch (error) {
             console.log(error);
         }
     }
     else if(key === 'Backspace'){
-        robot.keyToggle("backspace", "down");
+        robot.keyToggle("backspace", val);
     }
     else if(key === 'Delete'){
-        robot.keyToggle("delete", "down");
+        robot.keyToggle("delete", val);
     }
     else if(key === 'Escape'){
-        robot.keyToggle("escape", "down");
+        robot.keyToggle("escape", val);
     }
     else if(key === 'Tab'){
-        robot.keyToggle("tab", "down");
+        robot.keyToggle("tab", val);
     }
     else if(key === 'ArrowUp'){
-        robot.keyToggle("up", "down");
+        robot.keyToggle("up", val);
     }
     else if(key === 'ArrowDown'){
-        robot.keyToggle("down", "down");
+        robot.keyToggle("down", val);
     }
     else if(key === 'ArrowLeft'){
-        robot.keyToggle("left", "down");
+        robot.keyToggle("left", val);
     }
     else if(key === 'ArrowRight'){
-        robot.keyToggle("right", "down");
+        robot.keyToggle("right", val);
     }
     else if(key === 'Control'){
-        robot.keyToggle("control", "down");
+        robot.keyToggle("control", val);
     }
     else if(key === 'Alt'){
-        robot.keyToggle("alt", "down");
+        robot.keyToggle("alt", val);
     }
     else if(key === 'AltGraph'){
-        robot.keyToggle("alt", "down");
+        robot.keyToggle("alt", val);
     }
     else if(key === 'Shift'){
-        robot.keyToggle("shift", "down");
+        robot.keyToggle("shift", val);
     }
     else if(key === 'F1'){
-        robot.keyToggle("f1", "down");
+        robot.keyToggle("f1", val);
     }
     else if(key === 'F2'){
-        robot.keyToggle("f2", "down");
+        robot.keyToggle("f2", val);
     }
     else if(key === 'F3'){
-        robot.keyToggle("f3", "down");
+        robot.keyToggle("f3", val);
     }
     else if(key === 'F4'){
-        robot.keyToggle("f4", "down");
+        robot.keyToggle("f4", val);
     }
     else if(key === 'F5'){
-        robot.keyToggle("f5", "down");
+        robot.keyToggle("f5", val);
     }
     else if(key === 'F6'){
-        robot.keyToggle("f6", "down");
+        robot.keyToggle("f6", val);
     }
     else if(key === 'F7'){
-        robot.keyToggle("f7", "down");
+        robot.keyToggle("f7", val);
     }
     else if(key === 'F8'){
-        robot.keyToggle("f8", "down");
+        robot.keyToggle("f8", val);
     }
     else if(key === 'F9'){
-        robot.keyToggle("f9", "down");
+        robot.keyToggle("f9", val);
     }
     else if(key === 'F10'){
-        robot.keyToggle("f10", "down");
+        robot.keyToggle("f10", val);
     }
     else if(key === 'F11'){
-        robot.keyToggle("f11", "down");
+        robot.keyToggle("f11", val);
     }
     else if(key === 'F12'){
-        robot.keyToggle("f12", "down");
+        robot.keyToggle("f12", val);
     }
     else if(key == 'PageUp'){
-        robot.keyToggle("pageup", "down");
+        robot.keyToggle("pageup", val);
     }
     else if(key == 'PageDown'){
-        robot.keyToggle("pagedown", "down");
+        robot.keyToggle("pagedown", val);
     }
     else if(key == 'Home'){
-        robot.keyToggle("home", "down");
+        robot.keyToggle("home", val);
     }
     else if(key == 'End'){
-        robot.keyToggle("end", "down");
-    }
-    else if(key == 'Insert'){
-        robot.keyToggle("insert", "down")
+        robot.keyToggle("end", val);
     }
     else{ 
         try {
-            robot.keyToggle(key, "down"); 
-        } catch (error) {
-            console.log('Nie obsłużono klawisza: '+key);
-        }
-    }
-}
-
-function upKey(key){
-    if(key === 'Enter'){ 
-        try {
-            robot.keyToggle("enter", "up");
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    else if(key === 'Backspace'){
-        robot.keyToggle("backspace", "up");
-    }
-    else if(key === 'Delete'){
-        robot.keyToggle("delete", "up");
-    }
-    else if(key === 'Escape'){
-        robot.keyToggle("escape", "up");
-    }
-    else if(key === 'Tab'){
-        robot.keyToggle("tab", "up");
-    }
-    else if(key === 'ArrowUp'){
-        robot.keyToggle("up", "up");
-    }
-    else if(key === 'ArrowDown'){
-        robot.keyToggle("down", "up");
-    }
-    else if(key === 'ArrowLeft'){
-        robot.keyToggle("left", "up");
-    }
-    else if(key === 'ArrowRight'){
-        robot.keyToggle("right", "up");
-    }
-    else if(key === 'Control'){
-        robot.keyToggle("control", "up");
-    }
-    else if(key === 'Alt'){
-        robot.keyToggle("alt", "up");
-    }
-    else if(key === 'AltGraph'){
-        robot.keyToggle("alt", "up");
-    }
-    else if(key === 'Shift'){
-        robot.keyToggle("shift", "up");
-    }
-    else if(key === 'F1'){
-        robot.keyToggle("f1", "up");
-    }
-    else if(key === 'F2'){
-        robot.keyToggle("f2", "up");
-    }
-    else if(key === 'F3'){
-        robot.keyToggle("f3", "up");
-    }
-    else if(key === 'F4'){
-        robot.keyToggle("f4", "up");
-    }
-    else if(key === 'F5'){
-        robot.keyToggle("f5", "up");
-    }
-    else if(key === 'F6'){
-        robot.keyToggle("f6", "up");
-    }
-    else if(key === 'F7'){
-        robot.keyToggle("f7", "up");
-    }
-    else if(key === 'F8'){
-        robot.keyToggle("f8", "up");
-    }
-    else if(key === 'F9'){
-        robot.keyToggle("f9", "up");
-    }
-    else if(key === 'F10'){
-        robot.keyToggle("f10", "up");
-    }
-    else if(key === 'F11'){
-        robot.keyToggle("f11", "up");
-    }
-    else if(key === 'F12'){
-        robot.keyToggle("f12", "up");
-    }
-    else if(key == 'PageUp'){
-        robot.keyToggle("pageup", "up");
-    }
-    else if(key == 'PageDown'){
-        robot.keyToggle("pagedown", "up");
-    }
-    else if(key == 'Home'){
-        robot.keyToggle("home", "up");
-    }
-    else if(key == 'End'){
-        robot.keyToggle("end", "up");
-    }
-    else if(key == 'Insert'){
-        robot.keyToggle("insert", "up")
-    }
-    else{ 
-        try {
-            robot.keyToggle(key, "up"); 
+            robot.keyToggle(key, val); 
         } catch (error) {
             console.log('Nie obsłużono klawisza: '+key);
         }
