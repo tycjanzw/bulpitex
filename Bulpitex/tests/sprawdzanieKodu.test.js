1c09d9e01f5d23b00fa596047eaf577d7e7dbbd3
piotrstukus@outlook.com <piotrstukus@outlook.com>
2021-12-12 19:25:56 +0100

    Test - v2

--- Bulpitex/tests/sprawdzanieKodu.test.js
+++ Bulpitex/tests/sprawdzanieKodu.test.js
@@ -1,12 +1,6 @@
-function generateCode(){
-    var code = '';
-    for(let i=0; i<10; i++)
-        code += Math.floor(Math.random()*10);
-    return code;
-}
-
 var tablicaKodow = [];
-var tablicaKodow2 = ['3251498897', '2864815301', '5789653010', '2554625385', '0735838523'];
+var tablicaKodow2 = ['3251498897', '2864815301', 
+    '5789653010', '2554625385', '0735838523'];
 
 function sprawdzKod(code, tab){
     var countOfCodes = 0;
@@ -19,20 +13,20 @@ function sprawdzKod(code, tab){
                 i = tab.length;
             }
         }
-        if(countOfCodes == 1)
-            return true;        
-        else
-            return false;
+        return countOfCodes == 1 ? true : false;
     }
 }
 
-test('kod jest poprawny', ()=>{
+test('Kod jest poprawny', ()=>{
     expect(sprawdzKod('3251498897', tablicaKodow)).toBe(false);
     expect(sprawdzKod('2864815301', tablicaKodow)).toBe(false);
     expect(sprawdzKod('5789653010', tablicaKodow)).toBe(false);
     expect(sprawdzKod('2554625385', tablicaKodow)).toBe(false);
+});
+
+test('Kod nie jest poprawny', ()=>{
     expect(sprawdzKod('0735838523', tablicaKodow)).toBe(false);
-    
+    expect(sprawdzKod('8457548526', tablicaKodow)).toBe(false);
     expect(sprawdzKod('3251498897', tablicaKodow2)).toBe(true);
     expect(sprawdzKod('2864815301', tablicaKodow2)).toBe(true);
     expect(sprawdzKod('5789653010', tablicaKodow2)).toBe(true);
